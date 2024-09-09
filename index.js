5a8364d729cfa37af0f4283be811a78cd0e72799
panchoto4largo <panchoto4largo@gmial.com>
2024-09-09 14:37:04 -0300

    cards modified by querySelector

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("¿Que ves? Todo bien")
+const h5 = document.querySelector('h5');
+const p = document.querySelector('p');
+const img = document.querySelector('img');
+
+h5.innerText = 'Creatina';
+p.innerText = 'Some quick example text to build on the card title and make up the bulk of the cards content.';
+img.setAttribute('src', 'https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg');
