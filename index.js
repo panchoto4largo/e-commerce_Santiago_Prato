ea9f2596810e8003c7361a6183d6159d3ca3f48e
panchoto4largo <panchoto4largo@gmial.com>
2024-09-12 22:41:17 -0300

    Function for the array

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ h1.innerText = 'Productos';
 
 let arr = [];
 
-for(let i = 1; i < 10; i++){
-    a = `
-        <div class="card m-3">
-                    <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top"  alt="Car ${i}">
-                    <div class="card-body">
-                        <h5 class="card-title">Car ${i}</h5>
-                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit donec, eleifend aenean purus dui ornare blandit risus facilisis interdum, cum nostra vulputate sem venenatis felis facilisi.</p>
-                        <p class="card-price">$2.100</p>
-                        <button type="button" class="btn btn-outline-dark">Buy item</button>
-                    </div>
-                </div>`
-    arr.push(a);
+function cards(arr){
+    for(let i = 1; i < 10; i++){
+        a = `
+            <div class="card m-3">
+                        <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top"  alt="Car ${i}">
+                        <div class="card-body">
+                            <h5 class="card-title">Car ${i}</h5>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipiscing elit donec, eleifend aenean purus dui ornare blandit risus facilisis interdum, cum nostra vulputate sem venenatis felis facilisi.</p>
+                            <p class="card-price">$2.100</p>
+                            <button type="button" class="btn btn-outline-dark">Ver más</button>
+                        </div>
+                    </div>`
+        arr.push(a);
+    }
+    section.innerHTML = arr.join().replaceAll(",", "");
 }
 
-section.innerHTML = arr.join().replaceAll(",", "");
+cards(arr)
