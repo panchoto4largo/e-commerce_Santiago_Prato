a7675c1ceec0632c5f4309c2780ce6451a606fb6
panchoto4largo <panchoto4largo@gmial.com>
2024-09-19 20:30:05 -0300

    Class to create a product

--- product/producto.js
+++ product/producto.js
@@ -1,13 +1,26 @@
 const main = document.querySelector('main');
 
-const objeto = {
-    titulo: "Juego", 
-    detalle: "Detalles",
-    precio: 200,
-    stock: 10,
-    imagen: "src",
+class Product {
+    constructor(titulo, detalle, precio, stock, imagen){
+        this.titulo = titulo;
+        this.detalle = detalle;
+        this.precio = precio;
+        this.stock = stock;
+        this.imagen = imagen;
+    }
 }
 
-objeto.stock = precio.stock - 1
+const objeto = new Product("Auto", "Buenisimo", 10000, 1, "https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/1.jpg");
 
-main.innerHTML = objeto
+const a = `
+            <div class="card m-3">
+                        <img src="${objeto.imagen}" class="card-img-top"  alt="Car 1">
+                        <div class="card-body">
+                            <h5 class="card-title">${objeto.titulo}</h5>
+                            <p class="${objeto.detalle}</p>
+                            <p class="card-price">${objeto.precio}</p>
+                            <p class="card-text">Quedan disponibles ${objeto.stock}</p>
+                        </div>
+                    </div>`;
+
+main.innerHTML = a
